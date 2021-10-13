fe2e49ec1ffe1722fea3e3b28f3e00f64f21d0a0
Philipe <philipe40@hotmail.com>
2021-10-13 08:49:30 -0300

    Sorting array before search

--- binarySearch.kt
+++ binarySearch.kt
@@ -1,5 +1,5 @@
 fun main() {
-	val array = arrayOf(1, 3, 4, 55, 104, 222, 229, 300)
+	val array = arrayOf(1, 3, 4, 55, 104, 222, 229, 300).sortedArray()
     val key = 55
     
     val search = binarySearch(array, key)
@@ -11,12 +11,12 @@ fun binarySearch(array: Array<Int>, key: Int): Int {
     var end = array.size-1
     
     while(begin <= end) {
-       val middle = (begin + end)/2
-       when {
-		   key == array[middle] -> return middle
-           key > array[middle] -> begin = middle + 1
-           else -> end = middle - 1
-       }
+        val middle = (begin + end)/2
+        when {
+            key == array[middle] -> return middle
+            key > array[middle] -> begin = middle + 1
+            else -> end = middle - 1
+        }
     }
     
     return -1
